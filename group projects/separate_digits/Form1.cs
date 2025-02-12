7a7942f52875c0c5314399771e5c5930b24ab98c
kdizzlle <112121179+kdizzlle@users.noreply.github.com>
2025-02-12 12:30:27 -0800

    update group project

--- group projects/separate_digits/Form1.cs
+++ group projects/separate_digits/Form1.cs
@@ -25,17 +25,30 @@ namespace separate_digits
         private void separate_Click(object sender, EventArgs e)
         {
             string str_number = number.Text;
-            int num = Convert.ToInt16(str_number);
-            int[] digit = new int[5]; //array for digits
-            for(int i = 0; i < 5; i++)
+            int num;
+            bool valid = int.TryParse(str_number, out num);//input validation
+            if (valid)
             {
-                //take the modulo of the number to get the least significant digit 
-                Digit[i] = number % 10;
-                // subtract the least sig. Digit from the number to get the remaining 4 digits 
-                Number = number – digit[i];
-                // divide by 10 to remove the 0 at the end from subtraction 
-                Number = number / 10;
+                num = Convert.ToInt32(str_number);
+                int[] digit = new int[5]; //array for digits
+                for (int i = 0; i < 5; i++)
+                {
+                    //take the modulo of the number to get the least significant digit 
+                    digit[i] = num % 10;
+                    // subtract the least sig. Digit from the number to get the remaining 4 digits 
+                    num -= digit[i];
+                    // divide by 10 to remove the 0 at the end from subtraction 
+                    num /= 10;
+                }
+                dig1.Text = Convert.ToString(digit[4]);
+                dig2.Text = Convert.ToString(digit[3]);
+                dig3.Text = Convert.ToString(digit[2]);
+                dig4.Text = Convert.ToString(digit[1]);
+                dig5.Text = Convert.ToString(digit[0]);
             }
+            else if (!valid)
+                MessageBox.Show("Invalid input - whole numbers only");
+            
 
 {
 
