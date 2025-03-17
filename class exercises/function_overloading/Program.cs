ca5081ac671bba728441544c8f9bc2a66e14e9e6
kdizzlle <112121179+kdizzlle@users.noreply.github.com>
2025-03-17 14:08:18 -0700

    class update

---
+++ class exercises/function_overloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function_overloading
+{
+    class Program
+    {
+        //input validation function
+        static int GetInput(string item, int a, int b)
+        {
+            int x = 0;
+            bool valid = true;
+            do
+            {
+                Console.WriteLine("Please enter " + item + " : ");
+                valid = int.TryParse(Console.ReadLine(), out x);
+                if (!valid)
+                    Console.WriteLine("Invalid! " + item + " has to be an integer! Please re-enter.");
+                else if (x < a)
+                    Console.WriteLine("Invalid! " + item + " cannot be less than " + a + ". Please re-enter.");
+                else if (x > b)
+                    Console.WriteLine("Invalid! " + item + " cannot be more than " + b + ". Please re-enter.");
+                else
+                    Console.WriteLine("You entered {0}",x);
+            } while (!valid || x < a || x > b);
+            return x;
+        }
+        static double GetInput(string item, double a)
+        {//no upper bound
+            double x = 0;
+            bool valid = true;
+            do
+            {
+                Console.WriteLine("Please enter " + item + " : ");
+                valid = double.TryParse(Console.ReadLine(), out x);
+                if (!valid)
+                    Console.WriteLine("Invalid! " + item + " has to be an double! Please re-enter.");
+                else if (x < a)
+                    Console.WriteLine("Invalid! " + item + " cannot be less than " + a + ". Please re-enter.");
+                else
+                    Console.WriteLine("You entered {0}", x);
+            } while (!valid || x < a);
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            int x = 10, y = 30;
+            int t = What(x, ref y);
+            Console.WriteLine("x={0}, y={1}, t={2}", x, y, t);
+            double weight1 = GetInput("your weight in lbs", 0, 1000);
+            double weight = GetInput("your weight in lbs", 0);
+            //int score = GetInput("the score", 0, 100);
+            Console.Read();
+        }
+        //gotta copy this from canvas i wasnt paying attention
+    }
+}
---
+++ class exercises/functions_avg-and-triple-app/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace functions_avg_and_triple_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            int[] test = new int[10];
+            test = ran(1, 10, 10);
+            for (int i = 0; i < 10; i++)
+                listBox1.Items.Add(test[i].ToString());
+            //MessageBox.Show("The average is: " + Ave(test).ToString());
+            listBox1.Items.Add("The average is: " + Ave(test).ToString());
+            int Idx = Array.IndexOf(test, 10);
+            if(Idx==-1)
+            {
+                for (int i = 0; i < 10; i++)
+                    listBox1.Items.Add("None of the elements is 10.");
+            }
+            else
+                listBox1.Items.Add("10 is the element of " + Idx.ToString());
+            //triple everything in box 1 and display them in box 2
+            int[] test3 = new int[10];
+            test3 = Triple(test);
+            for (int i = 0; i < 10; i++)
+                listBox2.Items.Add(test3[i].ToString());
+        }
+
+        //user-defined functions
+        int[] ran(int a, int b, int N)
+        {
+            Random rj = new Random();
+            int[] x = new int[N];
+            for (int i = 0; i < N; i++)
+                x[i] = rj.Next(a, b + 1);
+            return x;
+        }
+        int[] Triple(int[] x)
+        {
+            int N = x.Length;
+            int[] y = new int[N];
+            for (int i = 0; i < N; i++)
+                y[i] = 3 * x[i];
+            return y;
+        }
+        int Ave(int[] x)
+        {
+            int ave = 0;
+            for (int i=0; i < x.Length; i++)
+                ave += x[i];
+            ave /= x.Length;
+            return ave;
+        }
+    }
+}
